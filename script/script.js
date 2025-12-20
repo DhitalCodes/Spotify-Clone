ac607412cb18aaa124276597df130977db2c4c3a
Aaditya Dhital <AadityaDhital@kbc.edu.np>
2025-12-20 19:04:01 +0545

    Added a bit JS

--- script/script.js
+++ script/script.js
@@ -1,1 +1,38 @@
-console.log("Welcome to Spotify Clone");
+console.log("Lets Write Some JavaScript");
+
+async function getSongs(){
+    let a = await fetch("http://127.0.0.1:5500/songs/YBkhatra/");
+    let response = await a.text();
+    console.log(response);
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let songs = [];
+    for(let index = 0; index < as.length; index++){
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href);  // CORRECTED LINE
+        }
+    }
+    return songs;
+}
+
+async function main(){
+    // Get the list of all songs
+    let songs = await getSongs();
+    console.log(songs);
+
+    // Play first song
+    if(songs.length > 0){
+        var audio = new Audio(songs[0]);
+        audio.play();
+
+        audio.addEventListener("loadeddata", () => {
+            let duration = audio.duration;
+            console.log("Duration: " + duration);
+        });
+    } else {
+        console.log("No songs found");
+    }
+}
+main();
